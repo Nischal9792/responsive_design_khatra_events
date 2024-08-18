0a7e6c88e878e41700d4e2e9601f03c3bd2d5124
Nischal Chaudhary <nischalchaudhary476@gmail.com>
2024-08-18 11:46:55 +0545

    Responsive design of Khatra Events

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      fontFamily: {
+        'display': ['Poppins','sans-serif'],
+        'body': ['Inter','sans-serif'],
+      }
+    },
+  },
+  plugins: [],
+}
